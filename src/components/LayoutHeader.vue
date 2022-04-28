f1f34d18fd5adb100e63adc3dac53e9f68315751
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-04-28 23:43:32 +0300

    Style header

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="header">
+  <div class="header py-4 mb-4">
     <!-- <app-navbar [activeTab]="activeTab"></app-navbar> -->
     <div class="w-3/4 mx-auto">
-      <h2 class="text-left pl-4">IATI Validator</h2>
+      <h2 class="text-left pl-4 font-medium">IATI Validator</h2>
     </div>
   </div>
 </template>
 
 <style>
+.header {
+  background: linear-gradient(0deg, #124555 4%, #155366 20%);
+}
+.header::after {
+  content: '';
+  background-image: url('../assets/images/marque-white.png');
+  background-size: 70%;
+  background-repeat: no-repeat;
+  background-position: top right;
+  opacity: 0.25;
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
 .header h2 {
-  font-size: 60px;
+  font-size: 50px;
   border-left: 5px solid #81c3d6;
+  color: #ffffff;
 }
 </style>
