9fb7f3f4f34101aef8130610df1e6be2ec7af6d1
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-04-29 09:06:15 +0300

    Remove navbar-submenu styles

--- src/components/LayoutNavbar.vue
+++ src/components/LayoutNavbar.vue
@@ -14,11 +14,11 @@
       </button>
     </div>
     <div class="collapse navbar-collapse">
-      <div class="navbar-menu">
-        <a class="navbar-menu--item nav-link"> Home </a>
-        <a class="navbar-menu--item nav-link"> About Validator </a>
-        <a class="navbar-menu--item nav-link"> Check data </a>
-        <a class="navbar-menu--item nav-link"> Public data viewer </a>
+      <div class="navbar-menu flex justify-end">
+        <a href="/" class="navbar-menu--item nav-link"> Home </a>
+        <a href="#" class="navbar-menu--item nav-link"> About Validator </a>
+        <a href="#" class="navbar-menu--item nav-link"> Check data </a>
+        <a href="#" class="navbar-menu--item nav-link"> Public data viewer </a>
         <a
           href="https://developer.iatistandard.org/api-details#api=iati-validator-v2"
           target="_blank"
@@ -41,45 +41,15 @@
   box-shadow: 0 0 50px 0 rgba(0, 0, 0, 0.15);
 }
 
-@media (max-width: 1150px) {
-  .navbar {
-    flex-wrap: wrap;
-    margin-right: 2em;
-    margin-left: 2em;
-  }
-}
-
-@media (max-width: 600px) {
-  .navbar {
-    padding: 1em;
-  }
-}
-@media (max-width: 1150px) {
-  .navbar .navbar--header {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-  }
-}
-
 .navbar .navbar-menu {
-  display: flex;
   margin-top: 1.9rem;
   transition: 2s ease;
-  justify-content: flex-end;
-}
-
-@media (max-width: 1150px) {
-  .navbar .navbar-menu {
-    flex-direction: column;
-  }
 }
 
 .navbar-menu--item {
   display: block;
   position: relative;
   color: #121212;
-  position: relative;
   text-transform: uppercase;
   letter-spacing: 1px;
   font-size: 1rem;
@@ -87,11 +57,11 @@
   padding: 0;
 }
 
-.navbar-menu--item &:first-child {
+.navbar-menu--item:first-child {
   margin-left: 0;
 }
 
-.navbar-menu--item &::after {
+.navbar-menu--item::after {
   content: '';
   position: absolute;
   width: 100%;
@@ -104,21 +74,19 @@
   transition: transform 0.25s ease-out;
 }
 
-.navbar-menu--item &--active {
+.navbar-menu--item--active {
   transform: scaleX(1);
   transform-origin: bottom left;
 }
 
-.navbar-menu--item &--active &::after {
+.navbar-menu--item--active::after {
   transform: scaleX(1);
   transform-origin: bottom left;
 }
 
-.navbar-menu--item &:hover {
-  &::after {
-    transform: scaleX(1);
-    transform-origin: bottom left;
-  }
+.navbar-menu--item:hover::after {
+  transform: scaleX(1);
+  transform-origin: bottom left;
 }
 
 .navbar-brand {
@@ -141,50 +109,14 @@
   transition: 2s ease;
 }
 
-.navbar-collapse.collapse &.show {
+.navbar-collapse.collapse.show {
   display: block;
 }
 
-.navbar-submenu {
-  display: flex;
-  position: absolute;
-  padding: 1em 3em 1em 2em;
-  top: 0;
-  right: 0;
-  background-color: #e3e3e3;
-}
-
-.navbar-sudmenu__item {
-  position: relative;
-  color: #121212;
-  margin-left: 25px;
-  text-decoration: none;
-  font-size: 1.4rem;
-}
-
-.navbar-sudmenu__item &:first-child {
-  margin-left: 0;
-}
-
-.navbar-sudmenu__item &::after {
-  content: '';
-  position: absolute;
-  width: 100%;
-  transform: scaleX(0);
-  height: 1px;
-  bottom: 0;
-  left: 0;
-  background-color: #81c3d6;
-  transform-origin: bottom right;
-  transition: transform 0.25s ease-out;
-}
-
-.navbar-sudmenu__item &:hover &::after {
-  transform: scaleX(1);
-  transform-origin: bottom left;
-}
-
 @media (max-width: 600px) {
+  .navbar {
+    padding: 1em;
+  }
   .navbar-brand {
     width: 171px;
     height: 40px;
@@ -193,6 +125,19 @@
 }
 
 @media (max-width: 1150px) {
+  .navbar {
+    flex-wrap: wrap;
+    margin-right: 2em;
+    margin-left: 2em;
+  }
+  .navbar .navbar--header {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+  .navbar .navbar-menu {
+    flex-direction: column;
+  }
   .navbar-menu--item {
     margin-left: 0;
     text-align: center;
@@ -202,8 +147,7 @@
     display: inherit;
     border: none;
   }
-  .navbar-collapse.collapse,
-  .navbar-submenu {
+  .navbar-collapse.collapse {
     display: none;
   }
 }
