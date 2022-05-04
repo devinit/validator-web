5a4202cbd1a8540caf56093a0215c15451dac221
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-04 15:33:21 +0300

    Animate menu expansion and collapse

--- src/components/LayoutNavbar.vue
+++ src/components/LayoutNavbar.vue
@@ -16,8 +16,8 @@ const classes = {
   navbar:
     'max-w-[1200px] overflow-hidden z-5 bg-white shadow-md relative flex flex-wrap lg:flex-nowrap items-center justify-between mx-8 my-12 lg:mx-auto p-4 sm:py-4 sm:px-12',
   navbarIcon: 'relative block bg-no-repeat bg-contain bg-logo-colour m-0 w-44 h-10 sm:w-64 sm:h-14 sm:mb-auto',
-  navbarMenuWrapper: 'lg:block transition duration-2000 ease basis-full grow items-center',
-  navbarMenu: 'transition-height duration-500 ease-in-out mt-6 flex lg:justify-end flex-col lg:flex-row cursor-pointer',
+  navbarMenuWrapper: 'lg:block lg:h-auto transition-height duration-500 ease-in-out basis-full grow items-center',
+  navbarMenu: 'mt-6 flex lg:justify-end flex-col lg:flex-row cursor-pointer',
   menuItem: 'text-center mb-5 lg:mb-0 block relative uppercase tracking-widest text-base p-0 text-gray-7',
   menuItemAfter:
     'after:absolute after:w-full after:h-1 after:-bottom-5 after:left-0 after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-300 after:ease-out after:bg-primary after:hover:scale-x-100 after:hover:origin-bottom-left',
@@ -42,7 +42,7 @@ classes.menuItemAll = `${classes.menuItem} ${classes.menuItemAfter}`;
         <span class="inline-block bg-menu-icon w-6 h-6"></span>
       </button>
     </div>
-    <div :class="[classes.navbarMenuWrapper, showMobileNav ? 'block' : 'hidden']">
+    <div :class="[classes.navbarMenuWrapper, showMobileNav ? 'h-60' : 'h-0']">
       <div :class="[classes.navbarMenu]">
         <router-link to="/" :class="['ml-0', classes.menuItemAll, path === '/' ? classes.menuItemActive : '']">
           Home
