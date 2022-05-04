1d3988f3b18d5cff213bcf24679c4cf602b69ad1
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-04 17:13:46 +0300

    Style footer content wrapper

--- src/components/LayoutFooter.vue
+++ src/components/LayoutFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer class="bg-secondary">
-    <div>Footer Content Goes Here</div>
+  <footer class="block bg-secondary">
+    <div class="flex w-full justify-between px-2 py-10 lg:mx-auto lg:max-w-[1140px]">Footer Content Goes Here</div>
     <div>Footer Meta Goes Here</div>
   </footer>
 </template>
