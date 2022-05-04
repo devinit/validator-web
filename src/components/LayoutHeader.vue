aecb6742514a62bc7abf7c9e771d25370dff9586
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-04 16:40:44 +0300

    Switch LayoutHeader to tailwind

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -3,47 +3,14 @@
 </script>
 
 <template>
-  <div class="header pb-8 mb-4">
+  <div
+    class="relative mb-4 -mt-48 bg-gradient-iati pb-8 pt-60 after:absolute after:top-0 after:right-0 after:block after:h-full after:w-full after:bg-header-image after:bg-70% after:bg-right-top after:bg-no-repeat after:opacity-25"
+  >
     <LayoutNavbar />
-    <div class="main-title w-3/4 mx-auto">
-      <h2 class="text-left pl-4 font-medium">IATI Validator</h2>
+    <div class="mx-auto w-full pl-3.5 sm:w-3/4">
+      <h2 class="border-l-4 border-solid border-primary pl-4 text-left text-4xl font-medium text-white lg:text-5xl">
+        IATI Validator
+      </h2>
     </div>
   </div>
 </template>
-
-<style>
-  .header {
-    background: linear-gradient(0deg, #124555 4%, #155366 20%);
-    position: relative;
-    margin-top: -12.5em;
-    padding-top: 15rem;
-  }
-  .header::after {
-    content: '';
-    background-image: url('../assets/images/marque-white.png');
-    background-size: 70%;
-    background-repeat: no-repeat;
-    background-position: top right;
-    opacity: 0.25;
-    position: absolute;
-    top: 0;
-    right: 0;
-    display: block;
-    width: 100%;
-    height: 100%;
-  }
-  .header h2 {
-    font-size: 50px;
-    border-left: 5px solid #81c3d6;
-    color: #ffffff;
-  }
-  @media (max-width: 600px) {
-    .header .main-title {
-      width: 100%;
-      padding-left: 15px;
-    }
-    .main-title h2 {
-      font-size: 45px;
-    }
-  }
-</style>
