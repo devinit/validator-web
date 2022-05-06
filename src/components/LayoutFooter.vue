c1aded48124576c2463a6b6c194e3ca98b7636bf
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-06 13:36:45 +0300

    Make footer responsive

--- src/components/LayoutFooter.vue
+++ src/components/LayoutFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="block bg-secondary">
-    <div class="flex w-full justify-between px-2 py-10 lg:mx-auto lg:max-w-[1140px]">
-      <div class="flex flex-col p-2">
+    <div class="flex w-full flex-col justify-between px-4 py-10 lg:mx-auto lg:max-w-[1140px] lg:flex-row lg:px-2">
+      <div class="flex flex-col items-center p-2 lg:items-start">
         <a href="https://iatistandard.org" class="h-20 w-80 bg-logo-white bg-contain bg-no-repeat"></a>
       </div>
       <div class="flex flex-col p-2">
         <h2 class="text-lg font-bold uppercase text-white">Useful links</h2>
-        <ul class="mt-0 mb-2 list-none pl-0 text-left text-white">
+        <ul class="mt-0 mb-2 list-none pl-0 text-center text-white lg:text-left">
           <li class="underline">
             <a href="https://iatistandard.org/en/contact/">Contact</a>
           </li>
@@ -15,7 +15,7 @@
           </li>
         </ul>
       </div>
-      <div class="flex flex-col p-2">
+      <div class="flex flex-col p-2 lg:flex-row">
         <div class="text-center text-white">
           <p class="m-0 text-sm">Part of the <b>IATI Unified Platform</b></p>
           <p class="m-0 text-sm">
@@ -31,8 +31,8 @@
       </div>
     </div>
     <div class="border-t border-tertiary px-6 py-4">
-      <div class="flex w-full justify-between lg:mx-auto lg:max-w-[1140px]">
-        <p class="float-left inline-block text-white">Copyright IATI 2021. All rights reserved</p>
+      <div class="flex w-full flex-col justify-between lg:mx-auto lg:max-w-[1140px] lg:flex-row">
+        <p class="lg-pb-0 float-left inline-block pb-4 text-white">Copyright IATI 2021. All rights reserved</p>
         <div class="float-right">
           <a href="https://twitter.com/IATI_aid" class="inline-block h-9 w-9 bg-twitter"></a>
           <a
