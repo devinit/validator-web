c4558ae256e609339147c97af933f22cf37b992b
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-06 14:01:51 +0300

    Move navbar to top of screen

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div
-    class="relative mb-4 -mt-48 bg-gradient-iati pb-8 pt-60 after:absolute after:top-0 after:right-0 after:block after:h-full after:w-full after:bg-header-image after:bg-70% after:bg-right-top after:bg-no-repeat after:opacity-25"
+    class="relative mb-4 -mt-48 bg-gradient-iati pb-8 pt-36 after:absolute after:top-0 after:right-0 after:block after:h-full after:w-full after:bg-header-image after:bg-70% after:bg-right-top after:bg-no-repeat after:opacity-25"
   >
     <LayoutNavbar />
     <div class="mx-auto w-full pl-3.5 sm:w-3/4">
