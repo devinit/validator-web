42f76d3ed996297fe0f0d1afc2e841429a3dcc42
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-10 15:50:37 +0300

    Add first paragraph to about page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,1 +1,11 @@
-<template>About Page Goes Here</template>
+<template>
+  <div class="m-auto my-0 max-w-[1088px] text-left">
+    <section class="my-11 px-7">
+      <p>
+        The IATI Validator checks if IATI data complies with the rules and guidance in the IATI Standard. Find
+        information on how to improve the quality of development and humanitarian IATI data on resources and results to
+        ensure it is accessible and useful.
+      </p>
+    </section>
+  </div>
+</template>
