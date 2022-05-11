4dd3e64beabfbac5fe458d907629f0231d20912e
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-11 19:34:40 +0300

    Move Spinner to OrganisationsList

---
+++ src/components/OrganisationsList.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import LoadingSpinner from '../components/LoadingSpinner.vue';
+  const props = defineProps({ isFetching: { type: Boolean, default: false } });
+</script>
+
+<template>
+  <div v-if="props.isFetching" class="mb-4 flex h-full flex-col">
+    <div class="flex items-center justify-center">
+      <LoadingSpinner />
+    </div>
+  </div>
+</template>
--- src/pages/OrganisationsPage.vue
+++ src/pages/OrganisationsPage.vue
@@ -3,7 +3,7 @@
   import { setPageTitle } from '../state';
   import ContentContainer from '../components/ContentContainer.vue';
   import { fetchOrganisations } from '../utils';
-  import LoadingSpinner from '../components/LoadingSpinner.vue';
+  import OrganisationsList from '../components/OrganisationsList.vue';
 
   setPageTitle('Public data viewer');
   const isFetching = ref(true);
@@ -23,10 +23,6 @@
       the public IATI data is regularly refreshed from the IATI Registry. New organisations and IATI files will not
       immediately be visible here, but after at most a day.
     </p>
-    <div v-if="isFetching" class="mb-4 flex h-full flex-col">
-      <div class="flex items-center justify-center">
-        <LoadingSpinner />
-      </div>
-    </div>
+    <OrganisationsList :is-fetching="isFetching" />
   </ContentContainer>
 </template>
