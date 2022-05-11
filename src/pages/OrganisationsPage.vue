123c2cde64908eb7559592a34682d16561abf34a
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-11 17:43:19 +0300

    Add description text to OrganisationsPage

--- src/pages/OrganisationsPage.vue
+++ src/pages/OrganisationsPage.vue
@@ -1,7 +1,16 @@
 <script setup>
   import { setPageTitle } from '../state';
+  import ContentContainer from '../components/ContentContainer.vue';
 
   setPageTitle('Public data viewer');
 </script>
 
-<template>Organisations Page Goes Here</template>
+<template>
+  <ContentContainer>
+    <p class="text-left">
+      Search for an organisation and access the validation results on their public IATI data. The organisation list and
+      the public IATI data is regularly refreshed from the IATI Registry. New organisations and IATI files will not
+      immediately be visible here, but after at most a day.
+    </p>
+  </ContentContainer>
+</template>
