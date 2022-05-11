40f00c704ae4f44031db790f918004e385e2d107
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-11 19:31:01 +0300

    Prevent macro no-undef warnings in composition setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   env: {
     node: true,
+    'vue/setup-compiler-macros': true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:vue/base', 'plugin:vue/vue3-recommended', 'prettier'],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
