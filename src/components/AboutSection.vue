5c9f7c1e62dd5b944bec1bfb7ca922c144204c9f
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-11 13:49:09 +0300

    Wrap AboutSection slot in text-base div

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="my-11 px-7">
     <h3 class="my-4 text-lg font-bold" :v-if="title">{{ title }}</h3>
-    <slot />
+    <div class="text-base">
+      <slot />
+    </div>
   </section>
 </template>
 
