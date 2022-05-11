225856a62c23462ac2aec16a67790c3cf95d1707
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-11 18:38:41 +0300

    Show loading indicator when fetching

--- src/pages/OrganisationsPage.vue
+++ src/pages/OrganisationsPage.vue
@@ -1,13 +1,16 @@
 <script setup>
-  import { onMounted } from 'vue';
+  import { onMounted, ref } from 'vue';
   import { setPageTitle } from '../state';
   import ContentContainer from '../components/ContentContainer.vue';
   import { fetchOrganisations } from '../utils';
+  import LoadingSpinner from '../components/LoadingSpinner.vue';
 
   setPageTitle('Public data viewer');
+  const isFetching = ref(true);
 
   onMounted(() => {
     fetchOrganisations().then((data) => {
+      isFetching.value = false;
       console.log(data);
     });
   });
@@ -15,10 +18,15 @@
 
 <template>
   <ContentContainer>
-    <p>
+    <p class="mb-4">
       Search for an organisation and access the validation results on their public IATI data. The organisation list and
       the public IATI data is regularly refreshed from the IATI Registry. New organisations and IATI files will not
       immediately be visible here, but after at most a day.
     </p>
+    <div v-if="isFetching" class="mb-4 flex h-full flex-col">
+      <div class="flex items-center justify-center">
+        <LoadingSpinner />
+      </div>
+    </div>
   </ContentContainer>
 </template>
