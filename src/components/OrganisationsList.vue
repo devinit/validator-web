a973b23f57bfac318a359c6b9e44fc4c568f12f1
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-12 19:51:15 +0300

    Improve reactive behaviour of OrganisationsList

--- src/components/OrganisationsList.vue
+++ src/components/OrganisationsList.vue
@@ -1,14 +1,20 @@
 <script setup>
-  import { ref, onBeforeUpdate } from 'vue';
+  import { computed, ref, onBeforeUpdate } from 'vue';
   import LoadingSpinner from '../components/LoadingSpinner.vue';
   import SearchFilter from './SearchFilter.vue';
+  import StyledLink from './StyledLink.vue';
 
   const props = defineProps({
     isFetching: { type: Boolean, default: false },
     organisations: { type: Array, default: () => [] },
   });
 
-  const organisations = ref(props.organisations || []);
+  const searchText = ref(null);
+  const organisations = computed(() =>
+    searchText.value && props.organisations
+      ? props.organisations.filter((org) => new RegExp(searchText.value, 'gi').test(org.title))
+      : props.organisations || []
+  );
 
   onBeforeUpdate(() => {
     if (!organisations.value && props.organisations) {
@@ -16,12 +22,7 @@
     }
   });
 
-  const filterOrganisations = (searchText) =>
-    props.organisations.filter((org) => new RegExp(searchText, 'gi').test(org.title));
-
-  const onSearch = (searchText) => {
-    organisations.value = searchText ? filterOrganisations(searchText) : props.organisations;
-  };
+  const onSearch = (text) => (searchText.value = text);
 </script>
 
 <template>
@@ -32,7 +33,12 @@
   </div>
   <div v-else class="mb-4">
     <SearchFilter placeholder="Organisation name" @on-search="onSearch" />
-    <div class="py-4">Found {{ organisations.length }} organisations.</div>
-    <hr />
+    <div class="py-4">
+      Found <b>{{ organisations.length }}</b> organisations.
+    </div>
+    <hr class="mb-4" />
+    <div v-for="organisation in organisations" :key="organisation.name">
+      <StyledLink :to="`/organisation/${organisation.name}`">{{ organisation.title }}</StyledLink>
+    </div>
   </div>
 </template>
