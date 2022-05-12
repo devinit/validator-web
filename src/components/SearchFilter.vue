75d0eef7bde6039239dc5b059867c3daaabab850
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-12 18:39:14 +0300

    Make search filter responsive

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -6,14 +6,14 @@
 </script>
 
 <template>
-  <div class="flex w-[500px] flex-row flex-wrap justify-around">
+  <div class="flex w-full flex-row flex-wrap justify-around sm:w-[500px]">
     <input
       type="text"
-      class="m-0 w-[350px] border px-4 py-3 text-xl focus-visible:outline-none"
+      class="m-0 w-full border px-4 py-3 text-xl focus-visible:outline-none sm:w-[350px]"
       :placeholder="props.placeholder"
       autofocus
     />
-    <button class="relative inline w-[150px] border-0 bg-iati-blue p-3 text-base uppercase">
+    <button class="relative mt-4 inline w-full border-0 bg-iati-blue p-3 text-base uppercase sm:mt-0 sm:w-[150px]">
       {{ props.buttonCaption }}
     </button>
   </div>
