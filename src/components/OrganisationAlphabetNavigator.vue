f9348ba7b911997d34155c97304380afa279e4bb
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-13 00:56:52 +0300

    Navigate alphabetically via anchors

--- src/components/OrganisationAlphabetNavigator.vue
+++ src/components/OrganisationAlphabetNavigator.vue
@@ -1,10 +1,18 @@
 <script setup>
   const alphabet = Array.from(Array(26)).map((e, i) => String.fromCharCode(i + 65));
+
+  const navigate = (event) => {
+    const id = event.target.innerHTML;
+    const target = window.document.getElementById(id.toLowerCase());
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
 </script>
 
 <template>
   <div class="my-4 flex flex-row justify-between">
-    <span class="cursor-pointer">123</span>
-    <span v-for="letter in alphabet" :key="letter" class="cursor-pointer">{{ letter }}</span>
+    <span class="cursor-pointer" @click="navigate">123</span>
+    <span v-for="letter in alphabet" :key="letter" class="cursor-pointer" @click="navigate">{{ letter }}</span>
   </div>
 </template>
--- src/components/OrganisationsList.vue
+++ src/components/OrganisationsList.vue
@@ -1,9 +1,9 @@
 <script setup>
-  import { ref } from 'vue';
+  import { computed, ref } from 'vue';
+  import StyledLink from './StyledLink.vue';
   import LoadingSpinner from '../components/LoadingSpinner.vue';
   import OrganisationSearchFilter from './OrganisationSearchFilter.vue';
   import OrganisationAlphabetNavigator from './OrganisationAlphabetNavigator.vue';
-  import OrganisationsListItem from './OrganisationsListItem.vue';
 
   const props = defineProps({
     isFetching: { type: Boolean, default: false },
@@ -11,6 +11,17 @@
   });
 
   const organisations = ref(props.organisations || []);
+  const anchors = computed(() =>
+    (props.organisations || []).reduce((items, curr) => {
+      const firstChar = curr.title.split('')[0].toLowerCase();
+      return isNaN(Number(firstChar)) && !items.map((item) => item.anchor).includes(firstChar)
+        ? items.concat({ anchor: firstChar, name: curr.name })
+        : items;
+    }, [])
+  );
+
+  const getAnchor = (id) => anchors.value.find((item) => item.name === id);
+  const isAnchored = (id) => !!getAnchor(id);
 
   const onFilter = (results) => (organisations.value = results);
 </script>
@@ -28,10 +39,10 @@
       Found <b>{{ organisations.length }}</b> organisations.
     </div>
     <hr class="mb-2" />
-    <OrganisationsListItem
-      v-for="organisation in organisations"
-      :key="organisation.name"
-      :organisation="organisation"
-    />
+    <span :id="123"></span>
+    <div v-for="organisation in organisations" :key="organisation.name">
+      <span v-if="isAnchored(organisation.name)" :id="getAnchor(organisation.name).anchor"></span>
+      <StyledLink :to="`/organisation/${organisation.name}`">{{ organisation.title }}</StyledLink>
+    </div>
   </div>
 </template>
