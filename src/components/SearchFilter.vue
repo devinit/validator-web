2675d65ca7659c9c149895d802677b11efc00e98
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-13 01:16:43 +0300

    Search on button click

--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -1,25 +1,29 @@
 <script setup>
   import { ref, watch } from 'vue';
+
   const props = defineProps({
     buttonCaption: { type: String, default: 'Search' },
     placeholder: { type: String, default: '' },
   });
   const emit = defineEmits(['onSearch']);
   const search = ref(null);
-  watch(search, () => {
-    emit('onSearch', search.value);
-  });
+
+  watch(search, () => emit('onSearch', search.value));
+  const onSearch = () => emit('onSearch', search.value);
 </script>
 
 <template>
   <div class="flex w-full flex-row flex-wrap justify-around sm:w-[500px]">
     <input
       v-model="search"
-      class="m-0 w-full border px-4 py-3 text-xl focus-visible:outline-none sm:w-[350px]"
+      class="m-0 w-full border px-4 py-3 text-lg focus-visible:outline-none sm:w-[350px]"
       :placeholder="props.placeholder"
       autofocus
     />
-    <button class="relative mt-4 inline w-full border-0 bg-iati-blue p-3 text-base uppercase sm:mt-0 sm:w-[150px]">
+    <button
+      class="relative mt-4 inline w-full border-0 bg-iati-blue p-3 text-base uppercase sm:mt-0 sm:w-[150px]"
+      @click="onSearch"
+    >
       {{ props.buttonCaption }}
     </button>
   </div>
