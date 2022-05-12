52fa53a5c14e3a11815f075cb0c0bb8340ad4712
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-12 19:28:41 +0300

    Filter organisations

--- src/components/OrganisationsList.vue
+++ src/components/OrganisationsList.vue
@@ -1,10 +1,27 @@
 <script setup>
+  import { ref, onBeforeUpdate } from 'vue';
   import LoadingSpinner from '../components/LoadingSpinner.vue';
   import SearchFilter from './SearchFilter.vue';
+
   const props = defineProps({
     isFetching: { type: Boolean, default: false },
     organisations: { type: Array, default: () => [] },
   });
+
+  const organisations = ref(props.organisations || []);
+
+  onBeforeUpdate(() => {
+    if (!organisations.value && props.organisations) {
+      organisations.value = props.organisations;
+    }
+  });
+
+  const filterOrganisations = (searchText) =>
+    props.organisations.filter((org) => new RegExp(searchText, 'gi').test(org.title));
+
+  const onSearch = (searchText) => {
+    organisations.value = searchText ? filterOrganisations(searchText) : props.organisations;
+  };
 </script>
 
 <template>
@@ -14,6 +31,8 @@
     </div>
   </div>
   <div v-else class="mb-4">
-    <SearchFilter placeholder="Organisation name" />
+    <SearchFilter placeholder="Organisation name" @on-search="onSearch" />
+    <div class="py-4">Found {{ organisations.length }} organisations.</div>
+    <hr />
   </div>
 </template>
--- src/components/SearchFilter.vue
+++ src/components/SearchFilter.vue
@@ -1,14 +1,20 @@
 <script setup>
+  import { ref, watch } from 'vue';
   const props = defineProps({
     buttonCaption: { type: String, default: 'Search' },
     placeholder: { type: String, default: '' },
+  });
+  const emit = defineEmits(['onSearch']);
+  const search = ref(null);
+  watch(search, () => {
+    emit('onSearch', search.value);
   });
 </script>
 
 <template>
   <div class="flex w-full flex-row flex-wrap justify-around sm:w-[500px]">
     <input
-      type="text"
+      v-model="search"
       class="m-0 w-full border px-4 py-3 text-xl focus-visible:outline-none sm:w-[350px]"
       :placeholder="props.placeholder"
       autofocus
