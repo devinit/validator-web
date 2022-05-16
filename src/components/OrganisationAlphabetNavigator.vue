319def569664d555af941b8e392280f156133ede
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-16 22:09:35 +0300

    Rename from 123 to 0-9

--- src/components/OrganisationAlphabetNavigator.vue
+++ src/components/OrganisationAlphabetNavigator.vue
@@ -3,7 +3,7 @@
 
   const navigate = (event) => {
     const id = event.target.innerHTML;
-    const target = window.document.getElementById(id.toLowerCase());
+    const target = window.document.getElementById(id === '0 - 9' ? '123' : id.toLowerCase());
     if (target) {
       target.scrollIntoView({ behavior: 'smooth', block: 'center' });
     }
@@ -12,7 +12,7 @@
 
 <template>
   <div class="sticky top-0 hidden flex-row justify-between bg-white py-4 sm:flex">
-    <span class="cursor-pointer" @click="navigate">123</span>
+    <span class="cursor-pointer" @click="navigate">0 - 9</span>
     <span v-for="letter in alphabet" :key="letter" class="cursor-pointer" @click="navigate">{{ letter }}</span>
   </div>
 </template>
