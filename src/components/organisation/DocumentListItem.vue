c52beef99173e41908d9d171a59decd4ee5b7865
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-20 10:32:58 +0300

    Inline hasProperLink

--- src/components/organisation/DocumentListItem.vue
+++ src/components/organisation/DocumentListItem.vue
@@ -1,14 +1,13 @@
 <script setup>
   import format from 'date-fns/format';
   import { computed } from 'vue';
-  import { getDocumentFileName, hasProperyLink } from '../../utils/document';
+  import { getDocumentFileName, hasProperLink } from '../../utils/document';
 
   const props = defineProps({ document: { type: Object, default: () => {} } });
   const dateFormat = 'yyyy-MM-dd HH:mm (z)';
   const formatDate = (date) => (date ? format(new Date(date), dateFormat) : '');
 
   const fileName = computed(() => getDocumentFileName(props.document) || 'No filename available');
-  const renderDocumentLink = computed(() => hasProperyLink(props.document));
   const registryDate = computed(() => formatDate(props.document.modified || props.document.first_seen));
   const validationDate = computed(() => formatDate(props.document.validation_created));
 </script>
@@ -16,7 +15,7 @@
 <template>
   <tr class="odd:bg-white even:bg-slate-100">
     <td class="py-2 first:pl-3.5">
-      <a v-if="renderDocumentLink" :url="props.document.url" class="hover:underline">{{ fileName }}</a>
+      <a v-if="hasProperLink(props.document)" :url="props.document.url" class="hover:underline">{{ fileName }}</a>
       <span v-else>{{ fileName }}</span>
     </td>
     <td class="py-2">
