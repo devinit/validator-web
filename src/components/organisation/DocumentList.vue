ba271b0ec975af46615bdc4fd9b162ed4b45d4bc
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-23 20:26:31 +0300

    Add padding to document list

--- src/components/organisation/DocumentList.vue
+++ src/components/organisation/DocumentList.vue
@@ -2,10 +2,10 @@
   <table class="w-full table-auto">
     <thead class="border-y border-solid border-gray-300">
       <tr>
-        <th class="py-2.5 first:pl-3.5">File Name</th>
-        <th class="py-2.5">Identified in Registry</th>
-        <th class="py-2.5">Validated</th>
-        <th class="py-2.5">Validation Status</th>
+        <th class="py-2.5 pr-2.5 first:pl-3.5">File Name</th>
+        <th class="py-2.5 pr-2.5">Identified in Registry</th>
+        <th class="py-2.5 pr-2.5">Validated</th>
+        <th class="py-2.5 pr-2.5">Validation Status</th>
         <th class="py-2.5">Available in IATI Datastore</th>
       </tr>
     </thead>
