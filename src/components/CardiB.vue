839339e070bc84002e9b013d6c5f525107389f25
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-26 17:58:55 +0300

    Strip CardiB of BasicCard dependency

--- src/components/CardiB.vue
+++ src/components/CardiB.vue
@@ -1,19 +1,12 @@
 <script setup>
-  import BasicCard from './BasicCard.vue';
-  import CardHeader from './CardHeader.vue';
-
   const props = defineProps({ heading: { type: String, default: '' } });
 </script>
 
 <template>
-  <BasicCard class="shrink grow basis-auto rounded-none border-0 border-t-4 border-t-iati-green p-5 shadow-md">
-    <template v-if="props.heading" #header>
-      <CardHeader class="border-b border-dashed border-b-iati-green bg-white pt-0 text-center">
-        <h3 class="text-xl">{{ props.heading }}</h3>
-      </CardHeader>
-    </template>
-    <div class="-mx-3.5">
-      <slot />
-    </div>
-  </BasicCard>
+  <div class="m-2.5 flex shrink grow basis-auto flex-col border-t-4 border-t-iati-green p-5 shadow-lg">
+    <h3 class="mb-2 border-b border-dashed border-b-iati-green bg-white pb-2 text-center text-xl">
+      {{ props.heading }}
+    </h3>
+    <slot />
+  </div>
 </template>
