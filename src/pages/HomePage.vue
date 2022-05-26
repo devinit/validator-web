8673f25176455d908c39717ccdac75f8f5e854fb
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-26 17:59:39 +0300

    Add all Home Page cards

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,8 @@
 <script setup>
-  import ContentContainer from '../components/layout/ContentContainer.vue';
-  import StyledLink from '../components/StyledLink.vue';
   import CardiB from '../components/CardiB.vue';
+  import ContentContainer from '../components/layout/ContentContainer.vue';
+  import LinkButton from '../components/LinkButton.vue';
+  import StyledLink from '../components/StyledLink.vue';
 </script>
 
 <template>
@@ -21,24 +22,36 @@
     </p>
 
     <div class="-m-2.5 flex flex-wrap pt-4">
-      <CardiB heading="Check Data" class="flex w-16 flex-col"> Content Goes Here </CardiB>
-      <CardiB heading="Public Data Viewer" class="flex w-16 flex-col"> Content Goes Here </CardiB>
-      <CardiB heading="Public API" class="flex w-16 flex-col">
+      <CardiB heading="Check Data" class="w-[300px]">
+        <p>Check your IATI data</p>
+        <ul class="my-4 list-disc pl-10">
+          <li>Select and upload your IATI files OR add URL to files</li>
+          <li>Get validation results</li>
+        </ul>
+        <LinkButton to="/validate" class="text-tiny"> Check data </LinkButton>
+      </CardiB>
+      <CardiB heading="Public Data Viewer" class="w-[300px]">
+        <p>Check all files published on the IATI Registry</p>
+        <ul class="my-4 list-disc pl-10">
+          <li>Find an organisation</li>
+          <li>See the IATI files published by the organisation</li>
+          <li>Get validation results of an IATI file</li>
+        </ul>
+        <LinkButton to="/organisations" class="text-tiny"> Public data viewer </LinkButton>
+      </CardiB>
+      <CardiB heading="Public API" class="w-[300px]">
         <p>Validate IATI files or retrieve validation reports using the public IATI API</p>
         <ul class="my-4 list-disc pl-10">
           <li><code class="text-iati-code">GET</code> existing validation reports for published files</li>
           <li><code class="text-iati-code">POST</code> IATI XML and receive validation results</li>
         </ul>
-        <div class="w-full text-center">
-          <a
-            class="mx-auto inline-block bg-iati-green px-5 py-2 uppercase text-white hover:bg-iati-blue"
-            href="https://developer.iatistandard.org/api-details#api=iati-validator-v2"
-            target="_blank"
-            role="button"
-          >
-            Public API
-          </a>
-        </div>
+        <LinkButton
+          to="https://developer.iatistandard.org/api-details#api=iati-validator-v2"
+          :external="true"
+          class="text-tiny"
+        >
+          Public API
+        </LinkButton>
       </CardiB>
     </div>
   </ContentContainer>
