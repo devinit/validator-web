9b4bcdaa066a17e27a67c8dd1f44aebbf3427ba7
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-05-26 21:01:46 +0300

    Lock button text colour to white

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -9,7 +9,7 @@
 
 <template>
   <StyledLink
-    class="mx-auto mt-auto inline-block bg-iati-green px-5 py-2 uppercase text-white hover:bg-iati-blue"
+    class="mx-auto mt-auto inline-block bg-iati-green px-5 py-2 uppercase !text-white hover:bg-iati-blue"
     :to="props.to"
     :external="props.external"
     role="button"
