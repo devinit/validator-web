2af56230bb2cd41b23d5ff370029b4d437b47e1b
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-06-24 10:15:53 +0300

    Populate document info

--- src/components/report/DocumentInfo.vue
+++ src/components/report/DocumentInfo.vue
@@ -1,6 +1,16 @@
 <script setup>
+  import { formatDate } from '../../utils';
+  import { getDocumentDatastoreAvailability } from '../../utils/document';
   const props = defineProps({ document: { type: Object, default: null }, report: { type: Object, default: null } });
-  console.log(props);
 </script>
 
-<template>Info Goes Here</template>
+<template>
+  <div>
+    <span>IATI version: {{ props.report.iatiVersion }} | </span>
+    <span>Type: {{ props.report.fileType }} | </span>
+    <span>Report Generated: {{ formatDate(props.document.validation_created) }} | </span>
+    <span>
+      Available in Datastore: {{ getDocumentDatastoreAvailability({ ...props.document, report: props.report }) }}
+    </span>
+  </div>
+</template>
