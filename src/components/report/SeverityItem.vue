3edbce3ca7350562824cdd564adfa994c86813c7
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-06-28 17:53:45 +0300

    Fix severity background classes

--- src/components/report/SeverityItem.vue
+++ src/components/report/SeverityItem.vue
@@ -1,6 +1,12 @@
 <script setup>
   const props = defineProps({ severity: { type: Object, default: null } });
+  const bgClass = {
+    'bg-success': props.severity.id === 'success',
+    'bg-warning': props.severity.id === 'warning',
+    'bg-error': props.severity.id === 'error',
+    'bg-critical': props.severity.id === 'critical',
+  };
 </script>
 <template>
-  <div :class="['bg-' + props.severity.id]" class="px-4 py-2 text-white">{{ props.severity.name }}</div>
+  <div :class="bgClass" class="px-4 py-2 text-white">{{ props.severity.name }}</div>
 </template>
