14ae492466ed2df057841f442cfbd2e88be8f853
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-07-13 14:58:25 +0300

    Add CheckBox to SeverityItem

--- src/components/report/SeverityItem.vue
+++ src/components/report/SeverityItem.vue
@@ -1,4 +1,6 @@
 <script setup>
+  import CheckBox from '../CheckBox.vue';
+
   const props = defineProps({ severity: { type: Object, default: null } });
   const bgClass = {
     'bg-success': props.severity.id === 'success',
@@ -6,7 +8,20 @@
     'bg-error': props.severity.id === 'error',
     'bg-critical': props.severity.id === 'critical',
   };
+
+  const onFilter = (severity, checked = false) => {
+    console.log(checked ? 'Checked:' : 'Unchecked:', severity);
+  };
 </script>
 <template>
-  <div :class="bgClass" class="px-4 py-2 text-white">{{ props.severity.name }}</div>
+  <div :class="bgClass" class="px-4 py-2 text-white">
+    <CheckBox
+      :id="props.severity.name"
+      :label="props.severity.name"
+      :name="props.severity.name"
+      :checked="true"
+      @checked="onFilter(props.severity.name, true)"
+      @unchecked="onFilter(props.severity.name)"
+    />
+  </div>
 </template>
