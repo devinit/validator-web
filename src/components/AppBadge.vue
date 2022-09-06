49a7ff9b35e595d47f034595490dc9d9a5ee3dde
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-06 18:34:55 +0300

    Add AppBadge component

---
+++ src/components/AppBadge.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const props = defineProps({ variant: { type: String, default: 'default' } });
+
+  const colourClass = `bg-${props.variant === 'default' ? 'iati-blue' : props.variant}`;
+</script>
+
+<template>
+  <span
+    class="mr-1 inline-block whitespace-nowrap rounded px-1.5 py-1 text-center align-baseline text-[75%] font-bold leading-none text-white"
+    :class="colourClass"
+  >
+    <slot />
+  </span>
+</template>
