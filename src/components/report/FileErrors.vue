0c5b8a7b83d8a6a3fc72d0fd300648fa941eb0af
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-06 17:47:10 +0300

    Add title prop to FileErrors

--- src/components/report/FileErrors.vue
+++ src/components/report/FileErrors.vue
@@ -1,14 +1,14 @@
 <script setup>
   import AppAccordion from '../AppAccordion.vue';
 
-  const props = defineProps({ errors: { type: Object, default: null } });
+  const props = defineProps({ title: { type: String, default: '' }, errors: { type: Object, default: null } });
   console.log(props);
 </script>
 
 <template>
   <AppAccordion :open="true">
     <template #title>
-      <div class="w-full bg-slate-300 px-4 py-2 text-left">File Errors Go Here</div>
+      <div class="w-full bg-slate-300 px-4 py-2 text-left">{{ props.title }}</div>
     </template>
     <template #content>
       <div class="border border-gray-200 px-4">Content Goes Here</div>
