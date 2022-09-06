46f6df69ea1a8bc1cec2720fc071ea304ed2c074
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-06 17:45:10 +0300

    Add AppAccordion to FileErrors

--- src/components/report/FileErrors.vue
+++ src/components/report/FileErrors.vue
@@ -1,6 +1,17 @@
 <script setup>
+  import AppAccordion from '../AppAccordion.vue';
+
   const props = defineProps({ errors: { type: Object, default: null } });
   console.log(props);
 </script>
 
-<template><div>File Errors Go Here</div></template>
+<template>
+  <AppAccordion :open="true">
+    <template #title>
+      <div class="w-full bg-slate-300 px-4 py-2 text-left">File Errors Go Here</div>
+    </template>
+    <template #content>
+      <div class="border border-gray-200 px-4">Content Goes Here</div>
+    </template>
+  </AppAccordion>
+</template>
