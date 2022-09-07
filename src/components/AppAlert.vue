74571e973da415a1dab2f06d85706532fd3ad498
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-07 21:23:51 +0300

    Add AppAlert component

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const props = defineProps({ variant: { type: String, default: 'default' } });
+
+  const borderColour = {
+    'border-iati-blue': props.variant === 'default',
+    'border-success': props.variant === 'success',
+    'border-warning': props.variant === 'warning',
+    'border-error': props.variant === 'error',
+    'border-critical': props.variant === 'critical',
+  };
+</script>
+
+<template>
+  <div class="border-l-[6px] bg-slate-100 p-3" :class="borderColour"><slot /></div>
+</template>
