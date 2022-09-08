63b136dbea4064122f9008e56de684dae64ea623
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-08 17:22:54 +0300

    Add FeedbackItem component

---
+++ src/components/report/FeedbackItem.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import AppAlert from '../AppAlert.vue';
+
+  const props = defineProps({ item: { type: Object, default: null } });
+</script>
+
+<template>
+  <AppAlert class="mt-2" :variant="props.item.severity">
+    {{ props.item.message }}
+  </AppAlert>
+</template>
--- src/components/report/FeedbackList.vue
+++ src/components/report/FeedbackList.vue
@@ -1,8 +1,8 @@
 <script setup>
   import { getFeedbackCategoryLabel } from '../../utils';
-  import AppAlert from '../AppAlert.vue';
   import AppBadge from '../AppBadge.vue';
   import CenteredLoadingSpinner from '../CenteredLoadingSpinner.vue';
+  import FeedbackItem from './FeedbackItem.vue';
 
   const props = defineProps({ message: { type: Object, default: null } });
 </script>
@@ -11,8 +11,6 @@
   <CenteredLoadingSpinner v-if="!props.message" />
   <div v-else>
     <AppBadge>{{ getFeedbackCategoryLabel(props.message.category) }}</AppBadge>
-    <AppAlert v-for="item in props.message.errors" :key="item.id" class="mt-2" :variant="item.severity">
-      {{ item.message }}
-    </AppAlert>
+    <FeedbackItem v-for="item in props.message.errors" :key="item.id" :item="item" />
   </div>
 </template>
