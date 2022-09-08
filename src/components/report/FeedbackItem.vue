f3ac3219020e3e53b63784bc2fb8e4f7f7e1825c
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-08 16:51:11 +0300

    Show feedback item variant

--- src/components/report/FeedbackItem.vue
+++ src/components/report/FeedbackItem.vue
@@ -1,7 +1,8 @@
 <script setup>
   import { getFeedbackCategoryLabel } from '../../utils';
-  import CenteredLoadingSpinner from '../CenteredLoadingSpinner.vue';
+  import AppAlert from '../AppAlert.vue';
   import AppBadge from '../AppBadge.vue';
+  import CenteredLoadingSpinner from '../CenteredLoadingSpinner.vue';
 
   const props = defineProps({ message: { type: Object, default: null } });
 </script>
@@ -10,5 +11,8 @@
   <CenteredLoadingSpinner v-if="!props.message" />
   <div v-else>
     <AppBadge>{{ getFeedbackCategoryLabel(props.message.category) }}</AppBadge>
+    <AppAlert v-for="item in props.message.errors" :key="item.id" class="mt-2" :variant="item.severity">
+      {{ item.message }}
+    </AppAlert>
   </div>
 </template>
