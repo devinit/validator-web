b04f43fcc34df4777932518bbd714588a24bddda
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-08 18:33:28 +0300

    Add guidanceLink

--- src/components/report/FeedbackItem.vue
+++ src/components/report/FeedbackItem.vue
@@ -1,11 +1,36 @@
 <script setup>
+  import { computed, inject } from 'vue';
   import AppAlert from '../AppAlert.vue';
+  import { GUIDANCE_LINK_URL } from '../../utils';
 
-  const props = defineProps({ item: { type: Object, default: null } });
+  const props = defineProps({ item: { type: Object, default: null }, version: { type: String, default: '' } });
+  const guidanceLinks = inject('guidanceLinks');
+
+  const version = computed(() => props.version.replace('.', ''));
+  const isGuidanceAvailable = computed(
+    () => guidanceLinks.value && guidanceLinks.value.version && props.item.id in guidanceLinks.value.content
+  );
+  const guidanceLink = computed(() => {
+    if (isGuidanceAvailable.value) {
+      const { id } = props.item;
+      const { content } = guidanceLinks.value;
+      if ('url' in content[id]) {
+        return content[id].url;
+      }
+      if ('path' in content[id]) {
+        return `${GUIDANCE_LINK_URL}/${version.value}/${content[id].path}`;
+      }
+    }
+
+    return '';
+  });
 </script>
 
 <template>
   <AppAlert class="mt-2" :variant="props.item.severity">
+    <a v-if="isGuidanceAvailable" :href="guidanceLink" target="_blank" title="Open guidance in a new window">
+      (see guidance)
+    </a>
     {{ props.item.message }}
   </AppAlert>
 </template>
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,6 +1,7 @@
 export const SERVICES_URL = import.meta.env.VUE_ENV_VALIDATOR_SERVICES_URL;
 export const API_KEY_NAME = import.meta.env.VUE_ENV_VALIDATOR_SERVICES_KEY_NAME;
 export const API_KEY_VALUE = import.meta.env.VUE_ENV_VALIDATOR_SERVICES_KEY_VALUE;
+export const GUIDANCE_LINK_URL = import.meta.env.VUE_ENV_GUIDANCE_LINK_BASE_URL;
 
 export const buildServicesEndpoint = (endpoint) => `${SERVICES_URL}${endpoint}`;
 
