ee4225884451a744995c9926288e09fc229500c2
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-13 20:48:42 +0300

    Add badge and activity identifier

--- src/components/report/FeedbackGroup.vue
+++ src/components/report/FeedbackGroup.vue
@@ -1,13 +1,39 @@
 <script setup>
   import AppAccordion from '../AppAccordion.vue';
+  import AppBadge from '../AppBadge.vue';
+  import { getFileErrorsMessageTypeCount } from '../../utils';
 
   const props = defineProps({ activity: { type: Object, default: null } });
+  const messageTypes = ['critical', 'error', 'warning', 'info', 'success']
+    .map((messageType) => ({
+      type: messageType,
+      count: getFileErrorsMessageTypeCount(props.activity.errors, messageType),
+    }))
+    .filter((messageType) => messageType.count > 0);
+
+  const cleanIdentifier = (identifier) => {
+    const newLineIndex = identifier.indexOf('\n');
+    return newLineIndex !== -1 ? identifier.substring(0, newLineIndex) : identifier;
+  };
 </script>
 <template>
   <AppAccordion :open="false" class="mb-4">
     <template #title>
       <div class="w-full bg-slate-100 px-4 py-2 text-left">
-        {{ props.activity.title || 'Untitled Activity' }}
+        <div class="font-medium">
+          {{ props.activity.title || 'Untitled Activity' }}
+        </div>
+        <div class="text-sm">
+          {{ cleanIdentifier(props.activity.identifier) }}
+          <AppBadge
+            v-for="messageType in messageTypes"
+            :key="messageType.type"
+            :variant="messageType.type"
+            class="relative top-[-2px]"
+          >
+            {{ messageType.count }}
+          </AppBadge>
+        </div>
       </div>
     </template>
     <template #content>
