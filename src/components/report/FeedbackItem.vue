4e3ee1f38a36d1c1f4def5e36cf6df4fbbab0936
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-20 15:35:25 +0300

    Check that context is available

--- src/components/report/FeedbackItem.vue
+++ src/components/report/FeedbackItem.vue
@@ -47,7 +47,10 @@
         >
       </span>
       <span>{{ props.item.message }}</span>
-      <ul v-if="props.item.context.length > 1 || props.item.context[0].text !== ''" class="list-disc pl-10">
+      <ul
+        v-if="props.item.context.length > 1 || (props.item.context.length && props.item.context[0].text !== '')"
+        class="list-disc pl-10"
+      >
         <li v-for="(context, index) of props.item.context" :key="index">{{ context.text }}</li>
       </ul>
     </div>
