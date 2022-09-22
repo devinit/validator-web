28e747fc1d8bdb9f61a15d59f6b8cb2f7161ef12
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-22 21:12:44 +0300

    Add page description

--- src/pages/ValidatePage.vue
+++ src/pages/ValidatePage.vue
@@ -1,7 +1,12 @@
 <script setup>
+  import ContentContainer from '../components/layout/ContentContainer.vue';
   import { setPageTitle } from '../state';
 
   setPageTitle('Check data');
 </script>
 
-<template>Validate Page Goes Here</template>
+<template>
+  <ContentContainer class="pb-8">
+    <p class="mb-4">Upload your IATI file and receive validation feedback.</p>
+  </ContentContainer>
+</template>
