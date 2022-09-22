cc67e93ebb70fdafbe967e304e4dd8423ed32510
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-22 22:09:10 +0300

    Add radio buttons to manage validation file source

--- src/pages/ValidatePage.vue
+++ src/pages/ValidatePage.vue
@@ -1,12 +1,35 @@
 <script setup>
   import ContentContainer from '../components/layout/ContentContainer.vue';
   import { setPageTitle } from '../state';
+  import CheckBox from '../components/CheckBox.vue';
+  import { ref, watch } from 'vue';
 
   setPageTitle('Check data');
+
+  const fileSource = ref('upload');
+
+  watch(fileSource, () => {
+    console.log(fileSource.value);
+  });
 </script>
 
 <template>
   <ContentContainer class="pb-8">
     <p class="mb-4">Upload your IATI file and receive validation feedback.</p>
+    <CheckBox
+      id="upload"
+      label="I have a file(s) to upload"
+      name="fileSource"
+      type="radio"
+      :checked="true"
+      @checked="fileSource = 'upload'"
+    />
+    <CheckBox
+      id="remote"
+      type="radio"
+      label="I have a URL to a remote file(s)"
+      name="fileSource"
+      @checked="fileSource = 'remote'"
+    />
   </ContentContainer>
 </template>
