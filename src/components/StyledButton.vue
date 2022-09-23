f5e7ca1603481c920286c18be41dc2b3b896ea9f
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-09-23 08:57:00 +0300

    Add StyledButton

---
+++ src/components/StyledButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+  const emit = defineEmits(['click']);
+</script>
+
+<template>
+  <button
+    class="mx-auto mt-auto inline-block bg-iati-green px-5 py-2 uppercase !text-white hover:bg-iati-blue"
+    @click="emit('click')"
+  >
+    <slot />
+  </button>
+</template>
