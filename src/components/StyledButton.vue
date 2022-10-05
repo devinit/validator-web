b88f2e5659604baae4a9a0d4df558b97f5e75a44
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-10-05 11:42:35 +0300

    Add disabled styling to StyledButton

--- src/components/StyledButton.vue
+++ src/components/StyledButton.vue
@@ -4,7 +4,7 @@
 
 <template>
   <button
-    class="mx-auto mt-auto inline-block bg-iati-green px-5 py-2 uppercase !text-white hover:bg-iati-blue"
+    class="mx-auto mt-auto inline-block bg-iati-green px-5 py-2 uppercase !text-white hover:bg-iati-blue disabled:cursor-not-allowed disabled:opacity-75"
     @click="emit('click')"
   >
     <slot />
