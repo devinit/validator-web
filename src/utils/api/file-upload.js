b977a658d9b231daabce83c6861103bebee85e2f
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-10-05 11:46:14 +0300

    Add file-upload handler under api utils

---
+++ src/utils/api/file-upload.js
@@ -0,0 +1,18 @@
+import { last } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
+import { getDefaultServicesAPIOptions, SERVICES_URL } from '.';
+
+export const uploadFile = async (file, tmpWorkspaceId) => {
+  if (!file) {
+    return null;
+  }
+
+  const url = `${SERVICES_URL}/pvt/adhoc/upload?sessionId=${tmpWorkspaceId}&filename=${file.name}&guid=${uuidv4()}`;
+  const uploadData = new FormData();
+  uploadData.append('file', file, file.name);
+
+  // const req = await window.fetch('POST', url, uploadData, { headers: this.authHeader });
+  const req = await window.fetch(url, { ...getDefaultServicesAPIOptions(), method: 'post', body: uploadData });
+
+  return last(req.json());
+};
