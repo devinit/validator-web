42d06ad1fe6e8a666bb9c41b8316a02bc231649c
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-10-05 08:33:29 +0300

    Change border colour when step is not active

--- src/components/LocalFilesValidator.vue
+++ src/components/LocalFilesValidator.vue
@@ -13,7 +13,7 @@
 </script>
 <template>
   <div class="-m-2.5 flex flex-wrap pt-4">
-    <CardiB heading="Step 1" class="w-[300px]">
+    <CardiB heading="Step 1" class="w-[300px]" :class="{ 'border-t-iati-blue': activeStep !== 1 }">
       <p class="text-center">Select your IATI files. You can select multiple files at the same time.</p>
       <FileInputButton
         accept=".xml"
@@ -24,11 +24,19 @@
         Browse
       </FileInputButton>
     </CardiB>
-    <CardiB heading="Step 2" class="w-[300px]" :class="{ 'pointer-events-none opacity-50': activeStep === 1 }">
+    <CardiB
+      heading="Step 2"
+      class="w-[300px]"
+      :class="{ 'pointer-events-none opacity-50': activeStep === 1, 'border-t-iati-blue': activeStep !== 2 }"
+    >
       <p class="text-center">Upload your IATI files and start validation.</p>
       <LinkButton to="/validate" class="text-tiny"> Upload </LinkButton>
     </CardiB>
-    <CardiB heading="Step 3" class="w-[300px]" :class="{ 'pointer-events-none opacity-50': activeStep !== 3 }">
+    <CardiB
+      heading="Step 3"
+      class="w-[300px]"
+      :class="{ 'pointer-events-none opacity-50': activeStep !== 3, 'border-t-iati-blue': activeStep !== 3 }"
+    >
       <p class="mb-4 text-center">
         Your files are being validated - click below to view their progress and, when complete, reports.
       </p>
