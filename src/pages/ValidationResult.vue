aebf6a67423b2a39f02ab1d140a09b936b8cc557
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-10-05 12:23:09 +0300

    Add ValidationResult page

---
+++ src/pages/ValidationResult.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { ref } from 'vue';
+  import { useRoute } from 'vue-router';
+  import ContentContainer from '../components/layout/ContentContainer.vue';
+  import { setPageTitle } from '../state';
+
+  setPageTitle('Validation results');
+  const route = useRoute();
+
+  const tempWorkspaceID = ref(route.params.tempWorkspaceID);
+
+  console.log(tempWorkspaceID);
+</script>
+
+<template>
+  <ContentContainer class="pb-8">
+    <p class="mb-4">Your personal workspace is</p>
+  </ContentContainer>
+</template>
--- src/router.js
+++ src/router.js
@@ -5,12 +5,13 @@ import OrganisationPage from './pages/OrganisationPage.vue';
 import OrganisationsPage from './pages/OrganisationsPage.vue';
 import ReportPage from './pages/ReportPage.vue';
 import ValidatePage from './pages/ValidatePage.vue';
+import ValidationResult from './pages/ValidationResult.vue';
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/about', component: AboutPage },
   { path: '/validate', component: ValidatePage },
-  { path: '/validate/:tempWorkspaceID', component: ValidatePage },
+  { path: '/validate/:tempWorkspaceID', component: ValidationResult },
   { path: '/organisations', component: OrganisationsPage },
   { path: '/organisation/:name', component: OrganisationPage },
   { path: '/report/:id', component: ReportPage },
