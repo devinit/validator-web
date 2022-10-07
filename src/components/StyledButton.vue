da184308f68f1071990b9fa8523e05e56a19dcab
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-10-07 12:06:25 +0300

    Add outline styled button

--- src/components/StyledButton.vue
+++ src/components/StyledButton.vue
@@ -1,10 +1,18 @@
 <script setup>
+  const props = defineProps({ outline: { type: Boolean, default: false } });
   const emit = defineEmits(['click']);
+
+  const classes = {
+    'bg-iati-green !text-white hover:bg-iati-blue': !props.outline,
+    'bg-white !text-iati-green hover:bg-iati-green hover:!text-white border-iati-green border border-solid':
+      props.outline,
+  };
 </script>
 
 <template>
   <button
-    class="mx-auto mt-auto inline-block bg-iati-green px-5 py-2 uppercase !text-white hover:bg-iati-blue disabled:cursor-not-allowed disabled:opacity-75"
+    class="mx-auto mt-auto inline-block px-5 py-2 uppercase disabled:cursor-not-allowed disabled:opacity-75"
+    :class="classes"
     @click="emit('click')"
   >
     <slot />
