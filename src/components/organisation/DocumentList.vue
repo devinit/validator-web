ae6cb9cbc310024c9485f830f0eaa30d1e915a83
ThatcherK <kaliithatmau@gmail.com>
2022-10-12 20:37:55 +0300

    Remove unused sorting code

--- src/components/organisation/DocumentList.vue
+++ src/components/organisation/DocumentList.vue
@@ -1,16 +1,6 @@
 <script setup>
-  import { reactive } from 'vue';
   import { getSortDirection, getSortValue, processedTableDocumentFields, sortOptions } from '../../utils';
   import DocumentListItem from './DocumentListItem.vue';
-
-  const state = reactive({
-    sortKey: '',
-    sortDirection: '',
-  });
-  function onSortKeyChange(sortVariable, direction) {
-    state.sortKey = sortVariable;
-    state.sortDirection = direction;
-  }
 
   const headerClassNames = 'hidden border-y border-solid border-gray-300 p-2.5 font-bold sm:block';
   const props = defineProps({
@@ -18,7 +8,6 @@
     sortvariable: { type: String, default: '' },
   });
   const documentSort = () => {
-    console.log('running');
     return processedTableDocumentFields(
       props.documents,
       getSortValue(props.sortvariable, sortOptions),
@@ -30,115 +19,11 @@
 <template>
   <div class="grid grid-cols-1">
     <div class="sticky top-0 grid grid-cols-5 gap-0 bg-white">
-      <div class="first:pl-3.5" :class="headerClassNames">
-        File Name
-        <span>
-          <svg
-            class="h-6 w-6 text-gray-500"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            @click="onSortKeyChange('fileName', 'ascending')"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" />
-            <path d="M18 15l-6-6l-6 6h12" />
-          </svg>
-          <svg
-            class="h-6 w-6 text-gray-500"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            @click="onSortKeyChange('fileName', 'descending')"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" />
-            <path d="M18 15l-6-6l-6 6h12" transform="rotate(180 12 12)" />
-          </svg>
-        </span>
-      </div>
-      <div :class="headerClassNames">
-        Identified in Registry
-        <span>
-          <svg
-            class="h-6 w-6 text-gray-500"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            @click="onSortKeyChange('registryIdentity', 'ascending')"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" />
-            <path d="M18 15l-6-6l-6 6h12" />
-          </svg>
-          <svg
-            class="h-6 w-6 text-gray-500"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            @click="onSortKeyChange('registryIdentity', 'descending')"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" />
-            <path d="M18 15l-6-6l-6 6h12" transform="rotate(180 12 12)" />
-          </svg>
-        </span>
-      </div>
-      <div :class="headerClassNames">
-        Validated
-        <span>
-          <svg
-            class="h-6 w-6 text-gray-500"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            @click="onSortKeyChange('validationDate', 'ascending')"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" />
-            <path d="M18 15l-6-6l-6 6h12" />
-          </svg>
-          <svg
-            class="h-6 w-6 text-gray-500"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            @click="onSortKeyChange('validationDate', 'descending')"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" />
-            <path d="M18 15l-6-6l-6 6h12" transform="rotate(180 12 12)" />
-          </svg>
-        </span>
-      </div>
-      <div :class="headerClassNames" @click="onSortKeyChange('validationStatus')">Validation Status</div>
-      <div :class="headerClassNames" @click="onSortKeyChange('availabilityInDatastore')">
-        Available in IATI Datastore
-      </div>
+      <div class="first:pl-3.5" :class="headerClassNames">File Name</div>
+      <div :class="headerClassNames">Identified in Registry</div>
+      <div :class="headerClassNames">Validated</div>
+      <div :class="headerClassNames">Validation Status</div>
+      <div :class="headerClassNames">Available in IATI Datastore</div>
     </div>
     <DocumentListItem v-for="document in documentSort()" :key="document.hash" :document="document" />
   </div>
