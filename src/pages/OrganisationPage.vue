7274dd272b61e4bf54bb9e695e8476fe2fcc3bed
ThatcherK <kaliithatmau@gmail.com>
2022-10-17 17:56:27 +0300

    Override VueMultiSelect colors to match iati colors

--- src/pages/OrganisationPage.vue
+++ src/pages/OrganisationPage.vue
@@ -126,3 +126,18 @@
   </ContentContainer>
 </template>
 <style src="vue-multiselect/dist/vue-multiselect.css"></style>
+<style>
+  .multiselect__option--highlight,
+  .multiselect__option--highlight:after {
+    background: #155366;
+  }
+  .multiselect__option--selected.multiselect__option--highlight {
+    background: #81c3d6;
+  }
+  .multiselect__option--selected.multiselect__option--highlight:after {
+    background: #81c3d6;
+  }
+  .multiselect__option--group-selected.multiselect__option--highlight {
+    background: #81c3d6;
+  }
+</style>
