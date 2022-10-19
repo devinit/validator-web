ba168055bbd01a0435a7f98ff52ab169c2076daf
ThatcherK <kaliithatmau@gmail.com>
2022-10-19 14:34:29 +0300

    Change document fetch url to query api with document name

--- src/utils/api/documents.js
+++ src/utils/api/documents.js
@@ -1,6 +1,8 @@
 import { SERVICES_URL, getDefaultServicesAPIOptions } from '.';
 
-export const getDocumentURL = (documentIdentifier) => `${SERVICES_URL}/pvt/documents/${documentIdentifier}`;
+// export const getDocumentURL = (documentIdentifier) => `${SERVICES_URL}/pvt/documents/${documentIdentifier}`;
+export const getDocumentURL = (documentIdentifier) =>
+  `${SERVICES_URL}/pub/validation/existing?name=${documentIdentifier}`;
 export const fetchDocumentByID = async (documentID) => {
   const url = getDocumentURL(documentID);
   const response = await window.fetch(url, getDefaultServicesAPIOptions());
@@ -15,11 +17,13 @@ export const fetchDocumentByID = async (documentID) => {
 
 export const fetchDocumentByName = async (documentName) => {
   const url = getDocumentURL(documentName);
+  console.log(url);
   const response = await window.fetch(url, getDefaultServicesAPIOptions());
   if (response.status === 200) {
     const data = await response.json();
 
-    return data[0];
+    console.log(data);
+    return data;
   }
 
   return null;
@@ -29,6 +33,7 @@ export const validationReportURL = (documentName, isTestFile = false) =>
   `${SERVICES_URL}/pub/validation/existing?${isTestFile ? 'testfile' : 'name'}=${documentName}`;
 export const fetchValidationReport = async (documentName, isTestFile = false) => {
   const url = validationReportURL(documentName, isTestFile);
+  console.log(url);
   const response = await window.fetch(url, getDefaultServicesAPIOptions());
   if (response.status === 200) {
     const data = await response.json();
