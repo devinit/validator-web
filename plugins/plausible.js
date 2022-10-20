8ca575996fcbf2d93f79a37e705ddc1436823f81
akmiller01 <alex.k.miller@gmail.com>
2022-10-20 13:15:51 -0400

    Plugins

---
+++ plugins/plausible.js
@@ -0,0 +1,12 @@
+import Plausible from 'plausible-tracker';
+
+export default {
+  install: (app, options) => {
+    const { enableAutoPageviews, enableAutoOutboundTracking } = Plausible(options);
+
+    enableAutoPageviews();
+    enableAutoOutboundTracking();
+
+    app.provide('plausible');
+  },
+};
