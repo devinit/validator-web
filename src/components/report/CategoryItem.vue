fe9b7ac7c5060cd419a6bf8ec0bf2bf3143bdd18
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-10-28 18:01:41 +0300

    Update CategoryItem to handle clear filter

--- src/components/report/CategoryItem.vue
+++ src/components/report/CategoryItem.vue
@@ -1,10 +1,17 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref, watch } from 'vue';
   import CheckBox from '../CheckBox.vue';
 
   const props = defineProps({ category: { type: Object, default: null } });
   const emit = defineEmits(['select']);
   const checked = ref(true);
+
+  watch(
+    () => props.category,
+    () => {
+      checked.value = props.category ? props.category.show : checked.value;
+    }
+  );
 
   const onToggleCategory = () => {
     checked.value = !checked.value;
