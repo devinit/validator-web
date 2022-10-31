224a4ed4f74254a8918e2b49a510fa4266142532
ThatcherK <kaliithatmau@gmail.com>
2022-10-31 16:50:29 +0300

    Add labels to pagination buttons

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -5,11 +5,11 @@
 
 <template>
   <div class="px-4 text-center">
-    <button class="mr-1 rounded-md border border-slate-200 p-4" @click="emits('previous')">
+    <button class="mr-1 rounded-md border border-slate-200 p-4" aria-label="Previous" @click="emits('previous')">
       <IconChevron />
     </button>
     <slot />
-    <button class="ml-1 rounded-md border border-slate-200 p-4" @click="emits('next')">
+    <button class="ml-1 rounded-md border border-slate-200 p-4" aria-label="Next" @click="emits('next')">
       <IconChevron direction="right" />
     </button>
   </div>
