137959a73b74d48735896ff1eaeda5dc4cf9f4b8
nosvalds <niko@devinit.org>
2022-11-08 15:12:56 +0000

    name and script updates

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  Object.assign(process.env, loadEnv(mode, process.cwd() + "/envs", ""));
+  Object.assign(process.env, loadEnv(mode, process.cwd() + '/envs', ''));
 
   return {
     plugins: [vue()],
-    envDir: "./envs",
-    envPrefix: "VUE_ENV_",
+    envDir: './envs',
+    envPrefix: 'VUE_ENV_',
+    server: {
+      host: 'localhost',
+      port: 4200,
+    },
     define: {
       __VUE_PROD_DEVTOOLS__: !!process.env.VUE_ENV_DEV_TOOLS,
     },
