2bd4c4497c3bfed0520f3908a75994dcb56583a7
nosvalds <niko@devinit.org>
2022-11-15 16:10:01 +0000

    update banner to highlight new features

--- src/components/NotificationBanner.vue
+++ src/components/NotificationBanner.vue
@@ -4,7 +4,14 @@
 
 <template>
   <div v-if="showBanner" class="flex flex-col items-center justify-center bg-yellow-200 p-1" @click="toggleTooltip">
-    <p class="p-1 font-bold text-black">Planned Maintenance - Tuesday 15 November 2022 - 0900-1200 GMT (London)</p>
+    <p class="p-1 font-bold text-black">
+      New Features Launched - 15 November 2020 -
+      <StyledLink
+        to="https://iatistandard.org/en/news/easier-checks-for-data-errors-with-improved-iati-validator/"
+        :external="true"
+        >Announcement</StyledLink
+      >
+    </p>
     <p>
       <StyledLink to="https://github.com/IATI/validator-web/releases/tag/v2.5.0" :external="true">v2.5.0</StyledLink> of
       the Validator is here! All reports are being regenerated today, so please be patient while the new format becomes
