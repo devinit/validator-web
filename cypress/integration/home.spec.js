0b358e6b162edc65700df1ca6e9cb04819e914da
nosvalds <niko@devinit.org>
2023-01-09 12:01:33 +0000

    add e2e tests for homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,26 @@
+describe('The Validator Homepage', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('Shows the correct h1 element', () => {
+    cy.get('h1').should('have.text', 'IATI Validator');
+  });
+  it('Check Data button navigates to /validate', () => {
+    cy.get('a[role=button][href="/validate"]').click();
+    cy.url().should('includes', '/validate');
+  });
+  it('Public Data Viewer button navigates to /organisations', () => {
+    cy.get('a[role=button][href="/organisations"]').click();
+    cy.url().should('includes', '/organisations');
+  });
+  it('Public API button contains external link to Public API Documentation', () => {
+    cy.get('a[role=button][title="Public API"]')
+      .invoke('attr', 'href')
+      .should('eq', 'https://developer.iatistandard.org/api-details#api=iati-validator-v2');
+  });
+  it('Shows the version links', () => {
+    cy.contains('Web v');
+    cy.contains('Services v');
+    cy.contains('API v');
+  });
+});
