d84456f865eef4bbb168fd98987360ce9d00c303
nosvalds <niko@devinit.org>
2023-01-10 12:48:48 +0000

    click through to validation report and check that page

--- cypress/integration/checkData.spec.js
+++ cypress/integration/checkData.spec.js
@@ -10,7 +10,7 @@ describe('The Ad Hoc Validate Check Data page', () => {
     cy.contains('button', 'Upload').should('be.disabled');
     cy.contains('a', 'View Progress and Reports').parent().should('have.class', 'pointer-events-none');
   });
-  it('allows you to upload a file for validation', () => {
+  it('allows you to upload a file for validation and access the report', () => {
     cy.get('input[type=file').selectFile('cypress/fixtures/iati-act-no-errors.xml', { force: true });
     cy.contains('iati-act-no-errors.xml');
     cy.contains('button', 'Upload').should('not.be.disabled').click();
@@ -20,8 +20,14 @@ describe('The Ad Hoc Validate Check Data page', () => {
     cy.url().should('includes', '/validate/');
     cy.contains('Validation results');
     cy.contains('iati-act-no-errors.xml');
+    // have to use a defined wait due to the polling architecture: https://github.com/cypress-io/cypress/issues/7729
+    cy.wait(15000);
+    cy.get('.doc-list-item').eq(0).click();
+    cy.get('h1').should('have.text', 'IATI Validator').siblings().should('have.text', 'File validation report');
+    cy.contains('IATI version');
+    cy.contains('Type');
   });
-  it('allows you to upload from URL for validation', () => {
+  it('allows you to upload from URL for validation and access the report', () => {
     cy.contains('URL to a remote file').click();
     cy.get('#url').type(
       'https://raw.githubusercontent.com/IATI/IATI-Extra-Documentation/version-2.03/en/activity-standard/activity-standard-example-annotated.xml'
@@ -35,5 +41,11 @@ describe('The Ad Hoc Validate Check Data page', () => {
     cy.contains(
       'https://raw.githubusercontent.com/IATI/IATI-Extra-Documentation/version-2.03/en/activity-standard/activity-standard-example-annotated.xml'
     );
+    // have to use a defined wait due to the polling architecture: https://github.com/cypress-io/cypress/issues/7729
+    cy.wait(15000);
+    cy.get('.doc-list-item').eq(0).click();
+    cy.get('h1').should('have.text', 'IATI Validator').siblings().should('have.text', 'File validation report');
+    cy.contains('IATI version');
+    cy.contains('Type');
   });
 });
