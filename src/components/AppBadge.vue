409a99ee3485ac84280bc16467e5d998afbbdb23
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-02-13 13:57:13 +0300

    Make badge text visible

--- src/components/AppBadge.vue
+++ src/components/AppBadge.vue
@@ -12,7 +12,7 @@
 
 <template>
   <span
-    class="mr-1 inline-block whitespace-nowrap rounded px-1.5 py-1 text-center align-baseline text-[80%] font-bold leading-none text-white"
+    class="mr-1 inline-block whitespace-nowrap rounded px-1.5 py-1 text-center align-baseline text-[80%] font-bold leading-none"
     :class="bgClass"
   >
     <slot />
